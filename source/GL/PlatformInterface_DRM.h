6e7ceb52a120f18bf30dda459c0a6f2c880261ff
Richard Collins <richard@hex-edge.com>
2022-06-05 18:33:51 +0100

    Fixed another include error for DRM

--- source/GL/PlatformInterface_DRM.h
+++ source/GL/PlatformInterface_DRM.h
@@ -13,7 +13,7 @@
 #include <gbm.h>	// sudo apt install libgbm-dev // This is used to get the egl stuff going. DRM is used to do the page flip to the display. Goes.. DRM -> GDM -> GLES (I think)
 #include <drm_fourcc.h>
 #include "EGL/egl.h" // sudo apt install libegl-dev
-#include "GLES2/gl2.h" // sudo apt install libgles2-mesa-dev
+//#include "GLES2/gl2.h" // sudo apt install libgles2-mesa-dev
 
 #define EGL_NO_X11
 #define MESA_EGL_NO_X11_HEADERS
